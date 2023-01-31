1f16501bdcb7e0780afb9d09dbce7fe172d4309f
Rafael Aureliano da Silva <aureliano.dev@gmail.com>
2023-01-30 22:59:57 -0300

    Componente: Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,11 @@
 import Styles from './Header.module.css'
 
+import igniteLogo from '../assets/ignite-logo.svg'
+
 export function Header() {
     return(
         <header className={Styles.header}>
-            <strong >Ignite Feed</strong>
+            <img src={igniteLogo} alt="LogoTipo do ignite" />
         </header>
     )
 }
